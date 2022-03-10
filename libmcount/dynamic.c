6b5a2dd58813bf8a1c87061af79c5863c4ee6566
Clément Guidi <clement.guidi@polymtl.ca>
2022-03-10 14:42:08 -0500

    mcount: Measure total patching time

--- libmcount/dynamic.c
+++ libmcount/dynamic.c
@@ -727,6 +727,8 @@ static int do_dynamic_update(struct symtabs *symtabs, char *patch_funcs,
 {
 	struct uftrace_mmap *map;
 	char *def_mod;
+	struct timespec start, end;
+	long long duration;
 
 	if (!patch_funcs && !unpatch_funcs)
 		return 0;
@@ -745,6 +747,7 @@ static int do_dynamic_update(struct symtabs *symtabs, char *patch_funcs,
 	stats.nomatch = 0;
 	stats.unpatch = 0;
 
+	clock_gettime(CLOCK_MONOTONIC, &start);
 	for_each_map(symtabs, map) {
 		struct mcount_dynamic_info *mdi;
 
@@ -755,6 +758,10 @@ static int do_dynamic_update(struct symtabs *symtabs, char *patch_funcs,
 
 		update_func_matched(mdi, map);
 	}
+	clock_gettime(CLOCK_MONOTONIC, &end);
+
+	duration = (end.tv_sec - start.tv_sec) * 1000000000 + end.tv_nsec - start.tv_nsec;
+	pr_dbg3("patching duration: %lld ns\n", duration);
 
 	if (stats.failed + stats.skipped + stats.nomatch == 0) {
 		pr_dbg("patched all (%d) functions in '%s'\n",
